29bcd9235c5efc84ea2e18b9e9d45c8521794b0b
vaishnav <vanshparadkar00@gmail.com>
2024-06-01 18:56:02 +0530

    updated

---
+++ hello.java
@@ -0,0 +1,15 @@
+package localrepo;
+
+public class hello {
+     
+public static void main(String[] args) {
+    
+
+     System.out.println("new code");
+     System.out.println("vaishnav");
+      
+    
+}
+} 
+
+
